e7def1611aba4bd6f2758a9c04d09798a770d121
selene-cho <selene.cho29@gmail.com>
2023-05-22 01:46:10 +0900

    feat: 8393문제, 합

---
+++ ch1/basic-grammar3_1-8393.js
@@ -0,0 +1,25 @@
+// https://www.acmicpc.net/problem/8393
+// 제목: 합
+// 난이도: ⭐️
+// 유형: 기초문법
+// 추천 풀이시간: 10분
+
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+/* 나의 풀이 */
+let data = Number(input[0]); // 문자열을 숫자로 변환할 때, parseInt에 비하여 Number의 속도가 더 빠르게 동작
+let summary = 0;
+
+for (let i = 1; i <= data; i++) {
+  summary += i;
+}
+
+console.log(summary);
+
+/* 정답 예시 */
+// 등차수열의 합 공식 이용 (같은 차이가 나는 수들의 합)
+
+let n = Number(input[0]);
+
+console.log((n * (n + 1)) / 2);
