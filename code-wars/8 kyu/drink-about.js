467d22685f1934c5d574b8390e366493cf14878e
selene-cho <selene.cho29@gmail.com>
2023-07-07 00:43:55 +0900

    feat: drink-about

---
+++ code-wars/8 kyu/drink-about.js
@@ -0,0 +1,35 @@
+/**
+ * DESCRIPTION: 
+    Kids drink toddy.
+    Teens drink coke.
+    Young adults drink beer.
+    Adults drink whisky.
+  Make a function that receive age, and return what they drink.
+
+ * Rules:
+    Children under 14 old.
+    Teens under 18 old.
+    Young under 21 old.
+    Adults have 21 or more.
+ */
+
+function peopleWithAgeDrink(old) {
+  if (old < 14) {
+    return 'drink toddy';
+  } else if (old < 18) {
+    return 'drink coke';
+  } else if (old < 21) {
+    return 'drink beer';
+  } else if (21 <= old) {
+    return 'dirnk whisky';
+  }
+  return '';
+}
+
+// TEST
+console.log(peopleWithAgeDrink(13)); // drink toddy
+console.log(peopleWithAgeDrink(17)); // drink coke
+console.log(peopleWithAgeDrink(18)); // drink beer
+console.log(peopleWithAgeDrink(20)); // drink beer
+console.log(peopleWithAgeDrink(22)); // drink whisky
+console.log(peopleWithAgeDrink(30)); // drink whisky
